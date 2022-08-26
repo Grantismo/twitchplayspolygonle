c3339b0e2a18daf2f3a836acf992d29619522505
Grant Warman <grant.warman@gmail.com>
2022-08-25 23:38:26 -0600

    Added guess command skeleton

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -21,6 +21,7 @@ type AlertData = {
 }
 
 const ALERT_TYPE_GUESS = "guess";
+const ALERT_TYPE_GAME_END = "game_end";
 const ALERT_STATUS_SUCCESS = "success";
 const ALERT_STATUS_ERROR = "error";
 
@@ -159,36 +160,102 @@ class Game {
 //  //console.log(JSON.stringify(msg))
 //})
 type RunArgs = {
-  client: tmi.Client
   args: string[]
   userState: tmi.Userstate
-  game?: Game
 }
 
 type Job = {
   command: Command
-  userState: tmi.Userstate
-  args: string[]
+  runArgs: RunArgs
+}
+
+type CommandArgs = {
+  client: tmi.Client
+  game: Game
+  name: string
+  isInteractive: boolean
+  alias?: string
 }
 
 abstract class Command {
-  constructor(readonly name: string, readonly isInteractive: boolean) {}
+
+  client: tmi.Client
+  game: Game
+  name: string
+  isInteractive: boolean
+  alias?: string
+
+  constructor(readonly args: CommandArgs) {
+    this.client = args.client
+    this.game = args.game
+    this.name = args.name
+    this.isInteractive = args.isInteractive
+    this.alias = args.alias
+  }
+
+  async say(msg: string) {
+    return await this.client.say(config.TWITCH_CHANNEL, msg)
+  }
+
   abstract run(args: RunArgs): void 
 }
 
 class Hello extends Command {
-  async run({client, game, userState, args}: RunArgs) {
-    await client.say(config.TWITCH_CHANNEL, `hey ${userState.username}!`)
+  async run({userState, args}: RunArgs) {
+    this.say(`hey ${userState.username}!`)
+  }
+}
+
+class Discord extends Command {
+  async run({userState, args}: RunArgs) {
+    await this.say('Join the Polygonle community at https://discord.com/invite/TrVJMwzjKc')
+  }
+}
+
+class Twitter extends Command {
+  async run({userState, args}: RunArgs) {
+    await this.say('Follow us at https://twitter.com/PolygonleGame')
+  }
+}
+
+class Guess extends Command {
+  async run({userState, args}: RunArgs) {
+    if(args.length === 0) {
+      return await this.say(`${userState.username} Couldn\'t find your guess. Usage: !guess yourword`)
+    }
+    if(args.length > 1) {
+      return await this.say(`${userState.username} Please guess only one word at a time!. Usage: !guess yourword`)
+    }
+    // TODO check word length
+    const result = await this.game.guess(args[0]);
+    if(result.type === ALERT_TYPE_GUESS && result.status === ALERT_STATUS_ERROR) {
+      return await this.say(`${userState.username} ${result.message}`)
+    }
+    if(result.type === ALERT_TYPE_GAME_END) {
+
+    }
+    console.log(JSON.stringify(result));
+  }
+}
+
+
+//  !howtoplay
+//  !settings
+//  !wordlist
+//
+//  !guess
+//  !g (alias)
+//  !toggle setting name
+
+let allValidCommands: Set<string> = new Set([]);
+class ListCommands extends Command {
+  async run({userState, args}: RunArgs) {
+    await client.say(config.TWITCH_CHANNEL, [...allValidCommands].join(', '))
   }
 }
 
 
 
-const toBang = (name: string) => `!${name}`;
-//new Set(['!hello', '!help', '!commands', '!guess', '!discord', '!twitter']);
-const commands = [new Hello('hello', false)]
-const allValidCommands = new Set(commands.map((c: Command) => toBang(c.name)));
-const nameToCommands = new Map(commands.map((c: Command) => [toBang(c.name), c]));
 
 const client = new tmi.Client({
 	options: { debug: true },
@@ -198,6 +265,27 @@ const client = new tmi.Client({
 	},
 	channels: [ config.TWITCH_CHANNEL ]
 });
+
+const game = new Game()
+
+const commands = [
+  new Hello({client, game, name: 'hello', isInteractive: false}),
+  new Discord({client, game, name: 'discord', isInteractive: false}),
+  new Twitter({client, game, name: 'twitter', isInteractive: false}),
+  new ListCommands({client, game, name: 'commands', isInteractive: false}),
+  new Guess({client, game, name: 'guess', isInteractive: true, alias: 'g'})
+];
+
+const toBang = (name: string) => `!${name}`;
+const nameToCommands = new Map();
+commands.forEach((c) => {
+  nameToCommands.set(toBang(c.name), c)
+  if(c.alias) {
+    nameToCommands.set(toBang(c.alias), c)
+  }
+})
+
+allValidCommands = new Set(nameToCommands.keys());
 
 client.connect();
 
@@ -211,29 +299,27 @@ client.on('message', (channel: string, userState: tmi.Userstate, message: string
 	if(commandParts.length > 0 && commandParts[0].startsWith('!')) {
     const command = nameToCommands.get(commandParts[0])
     if(!command) {
-		  client.say(channel, 'Unknown command. Use !commands to list all available');
+		  client.say(channel, 'Unknown command. Type !commands to list all available commands');
       return;
     }
     const args = commandParts.slice(1)
     const runArgs = {userState, args};
     if(!(command!.isInteractive)) {
       // commands which don't interact with the game state can be run immediately.
-      command!.run({...runArgs, client})
+      command!.run(runArgs)
     } else {
       // commands which interact with the game need to be processed by processJobs below.
-      jobs.push({...runArgs, command: command!})
+      jobs.push({runArgs, command: command!})
     }
   }
 });
 
-const game = new Game();
 const processJobs = async () => {
   if(jobs.length === 0) {
-    console.log('no jobs found, polling for new commands')
     return await wait(JOB_POLL_DELAY)
   }
   const next = jobs.shift()! //dequeue
-  return await next.command.run({client, game, userState: next.userState, args: next.args})
+  return await next.command.run(next.runArgs)
 }
 
 //setInterval()
